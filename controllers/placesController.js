b419e32093a622fda1aa4b0c723c618b4b03b455
Carlos <githubjuanmanuel@gmail.com>
2024-08-28 09:57:38 -0500

    Nuevos Cambios

--- controllers/placesController.js
+++ controllers/placesController.js
@@ -3,9 +3,12 @@ const axios = require("axios");
 const { createPlace } = require("../models/placeModel");
 
 const {
-  placesDB,
-  getPlaceTripAvisor,
   fetchPlacesByType,
+  placesDB,
+  searchPlacesTripAdvisor,
+  getNearbyAttractions,
+  getCoordinates,
+  geocodeGoogleMaps,
 } = require("../helpers/helpers");
 
 //Se importa el modelo para la base de datos
@@ -17,92 +20,71 @@ const api_place_details = process.env.API_PLACE_DETAILS;
 //Función que va a servir para obtener la información de los lugares
 const getPlaces = async (req, res) => {
   // Se obtiene el lugar que viene del request
-  const { query, types } = req.body;
+  const { city, country, types } = req.body;
   //Validación en caso de no haber nada en el request
-  if (!query) {
-    return res.status(400).json({ error: "Query parameter is required" });
+  if (!city || !country || !types) {
+    return res.status(400).json({ error: "All parameters are required" });
   }
 
   try {
-    // Función para obtener las coordenadas del lugar proporcionado, se utiliza la api de geocoding
-    const geocodeResponse = await axios.get(api_geo_code, {
-      params: {
-        address: query,
-        key: process.env.API_KEY,
-      },
-    });
+    // Función para obtener las coordenadas del lugar proporcionado conla api de geocoding de google maps
+    // const geocodeResponse = geocodeGoogleMaps(city)
+
     //Se extrae un objeto con la latitud y logitud
-    const location = geocodeResponse.data.results[0].geometry.location;
+    // const location = geocodeResponse.data.results[0].geometry.location;
 
     //Se almacenan las cordenades en la variable
-    const locationStr = `${location.lat},${location.lng}`;
+    // const locationStr = `${location.lat},${location.lng}`;
 
     // Realizar búsquedas para cada tipo de lugar
-    const placeRequests = types.map((type) =>
-      fetchPlacesByType(type, locationStr)
-    );
+    // const placeRequests = types.map((type) =>
+    //   fetchPlacesByType(type, locationStr)
+    // );
     // Arreglo que almacena todos los resultados de las peticiones anteriores
-    const results = await Promise.all(placeRequests);
+    // const results = await Promise.all(placeRequests);
 
     // Aplanar la lista de resultados y obtener detalles de los lugares de la api place details
-    const places = results.flat().map(async (result) => {
-      const detailsResponse = await axios.get(api_place_details, {
-        params: {
-          place_id: result.place_id, //id del lugar
-          key: process.env.API_KEY,
-          language: 'en'
-        },
-      });
+    // const places = results.flat().map(async (result) => {
+    //   const detailsResponse = await axios.get(api_place_details, {
+    //     params: {
+    //       place_id: result.place_id, //id del lugar
+    //       key: process.env.API_KEY
+    //     },
+    //   });
 
-      let city,country,lat,long 
+    //   // Variable que extrae los resultados de la petición a la api
+    //   const detailsGoogle = detailsResponse.data.result;
 
-      // Variable que extrae los resultados de la petición a la api
-      const detailsGoogle = detailsResponse.data.result;
+    //   const address_components = detailsGoogle.address_components;
 
-      lat = detailsGoogle.geometry.location.lat
-
-      const address_components = detailsGoogle.address_components;
-      
-      long = detailsGoogle.geometry.location.lng
-
-      for (const component of address_components) {
-        if (component.types.includes("country")) {
-          country =component.long_name
-        } else if (component.types.includes("locality")) {
-          city = component.long_name
-        }
-      }
-      
-      await getPlaceTripAvisor(
-        detailsGoogle.name,
-        city,
-        country
-      )
-
-      //se crea un objeto place en base al modelo con la información extraida de la api
-      return createPlace(
-        detailsGoogle.name,
-        detailsGoogle.formatted_address,
-        detailsGoogle.rating ? detailsGoogle.rating : "No rating available",
-        detailsGoogle.website ? detailsGoogle.website : "Website not available",
-        detailsGoogle.formatted_phone_number
-          ? detailsGoogle.formatted_phone_number
-          : "Phone number not available",
-        detailsGoogle.editorial_summary
-          ? detailsGoogle.editorial_summary.overview
-          : "No description available",
-        detailsGoogle.types // Agregar el tipo de lugar al objeto
-      );
-    });
+    // return createPlace(
+    //   //se crea un objeto place en base al modelo con la información extraida de la api
+    //   detailsGoogle.name,
+    //   detailsGoogle.formatted_address,
+    //   detailsGoogle.rating ? detailsGoogle.rating : "No rating available",
+    //   detailsGoogle.website ? detailsGoogle.website : "Website not available",
+    //   detailsGoogle.formatted_phone_number
+    //     ? detailsGoogle.formatted_phone_number
+    //     : "Phone number not available",
+    //   detailsGoogle.editorial_summary
+    //     ? detailsGoogle.editorial_summary.overview
+    //     : "No description available",
+    //   detailsGoogle.types // Agregar el tipo de lugar al objeto
+    // );
+    // });
 
     //Se crea un arreglo que almacena todos lugares con su respectiva información
-    const placesWithDetails = await Promise.all(places);
+    const coordinates = await getCoordinates(city, country);
 
-    //Se llama la función para ingresar datos a la base de dato
-    // placesDB(placesWithDetails);
+    console.log(coordinates);
+
+    const places = await searchPlacesTripAdvisor(coordinates, types)
+
+
+    // const placesWithDetails = await Promise.all(places);
 
     //Se devuelven los resultados como un objeto json
-    res.json(placesWithDetails);
+    res.json(places);
   } catch (error) {
     //Manejo de errores
     console.error(error);
--- helpers/helpers.js
+++ helpers/helpers.js
@@ -1,33 +1,18 @@
 const axios = require("axios");
+const { DynamoDBClient, BatchWriteItemCommand } = require("@aws-sdk/client-dynamodb");
+const { marshall, unmarshall } = require('@aws-sdk/util-dynamodb');
 
-//Se importa el modelo creado
-const { createPlace } = require("../models/placeModel");
-const { query } = require("express");
+const dynamoClient = new DynamoDBClient({ region: process.env.AWS_REGION });
+
+const generarId = () => {
+  const random= Math.random().toString(36);
+  const fecha = Date.now().toString(36);
+
+  return random + fecha
+}
 
 const placesDB = async (places) => {
-  for (const place of places) {
-    const placeExist = await Place.findOne({ address: place.address });
 
-    console.log(place.description);
-
-    if (placeExist) {
-      // console.log('place exist');
-      continue;
-    }
-
-    try {
-      const newPlace = await Place.create({
-        name: place.name,
-        address: place.address,
-        rating: place.rating.toString(),
-        description: place.description,
-        type: place.type,
-      });
-      console.log("New place added to db");
-    } catch (error) {
-      console.log(error);
-    }
-  }
 };
 
 const fetchPlacesByType = async (type, locationStr) => {
@@ -41,8 +26,8 @@ const fetchPlacesByType = async (type, locationStr) => {
     },
   });
   return response.data.results
-    .filter((place) => place.rating >= 4)
-    .sort((a, b) => a - b)
+    .filter((place) => place.rating >= 4 && place.types.includes(type))
+    .sort((a, b) => b.rating - a.rating)
     .slice(0, 10); // Limitar a los primeros 10 resultados
 };
 
@@ -51,12 +36,11 @@ const cleanText = (text) => {
     console.log(text);
     console.error("Expected a string as input");
   }
-  text = text
+
+  return text
     .toLowerCase()
     .normalize("NFD")
     .replace(/[\u0300-\u036f]/g, "");
-  console.log(text);
-  return text;
 };
 
 const validationPlace = (name1, city1, country1, name2, city2, country2) => {
@@ -77,6 +61,62 @@ const validationPlace = (name1, city1, country1, name2, city2, country2) => {
     }
   } else {
     return false;
+  }
+};
+
+const geocodeGoogleMaps = async (city) => {
+  try {
+    const geocodeResponse = await axios.get(api_geo_code, {
+      params: {
+        address: city,
+        key: process.env.API_KEY,
+      },
+    });
+    return geocodeResponse;
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+const searchPlacesTripAdvisor = async (cordinates, type) => {
+  const api_key = process.env.API_KEY_TA;
+  const api_location_tripAdvisor = process.env.API_NEARBY_SEARCH_TA;
+  const dominio = process.env.NOMBRE_DOMINIO;
+
+  if (!cordinates || !type) {
+    console.error("Please send all information");
+  }
+  const endpoint = api_location_tripAdvisor;
+  const params = {
+    key: api_key,
+    latLong: cordinates,
+    category: type,
+  };
+
+  const headers = {
+    origin: dominio,
+    Referer: dominio, // Reemplaza con tu dominio autorizado
+  };
+
+  try {
+    const response = await axios.get(endpoint, { params, headers });
+    const places = Object.values(response.data).flat();
+    let placesWithDetails = [];
+    for (const place of places) {
+      if (place) {
+        placesWithDetails.push(
+          await getPlaceDetailsTripAdvisor(parseInt(place.location_id, 10))
+        );
+      }
+    }
+
+    return placesWithDetails;
+  } catch (error) {
+    console.error(
+      "Error al hacer la solicitud a la API de TripAdvisor:",
+      error.message
+    );
+    throw error; // Lanzar el error para manejarlo en otro lugar si es necesario
   }
 };
 
@@ -102,6 +142,7 @@ const getPlaceTripAvisor = async (name, city, country) => {
   try {
     const response = await axios.get(endpoint, { params, headers });
     const places = Object.values(response.data).flat();
+    let placeWithDetails;
     for (const place of places) {
       if (place.name && place.address_obj.city && place.address_obj.country) {
         if (
@@ -114,12 +155,14 @@ const getPlaceTripAvisor = async (name, city, country) => {
             country
           )
         ) {
-          getPlaceDetailsTripAdvisor(parseInt(place.location_id, 10));
+          placeWithDetails = await getPlaceDetailsTripAdvisor(
+            parseInt(place.location_id, 10)
+          );
         }
       }
     }
 
-    // return place;
+    return placeWithDetails;
   } catch (error) {
     console.error(
       "Error al hacer la solicitud a la API de TripAdvisor:",
@@ -149,10 +192,6 @@ const getPlaceDetailsTripAdvisor = async (id) => {
   try {
     const response = await axios.get(endpoint, { params, headers });
     const place = response.data;
-    // console.log(place);
-    // console.log(
-    //   "------------------------------------------------------------------------------------"
-    // );
     return place;
   } catch (error) {
     console.error(
@@ -170,13 +209,15 @@ const getNearbyAttractions = async (city) => {
   if (!city) {
     console.error("Please send the city name");
   }
+
   city = cleanText(city);
+
   const endpoint = api_location_tripAdvisor;
   const params = {
     key: api_key,
     searchQuery: city,
     category: "attractions",
-    radius: 5,
+    radius: 15,
     radiusUnit: "km",
   };
 
@@ -187,8 +228,11 @@ const getNearbyAttractions = async (city) => {
 
   try {
     const response = await axios.get(endpoint, { params, headers });
+
     const places = Object.values(response.data).flat();
+
     const attractionsPlaces = [];
+
     for (const place of places) {
       let details = await getPlaceDetailsTripAdvisor(place.location_id);
       delete details.rating_image_url;
@@ -197,9 +241,10 @@ const getNearbyAttractions = async (city) => {
       delete details.neighborhood_info;
       attractionsPlaces.push(details);
     }
+
     console.log(attractionsPlaces);
 
-    // return place;
+    return attractionsPlaces;
   } catch (error) {
     console.error(
       "Error al hacer la solicitud a la API de TripAdvisor:",
@@ -209,13 +254,44 @@ const getNearbyAttractions = async (city) => {
   }
 };
 
-// Función para buscar lugares de un tipo específico (por ejemplo buscar tipo hoteles)
-// getPlaceTripAvisor("HotelHotel Acqua Medellín", "Medellín", "Colombia");
-getNearbyAttractions("Medellin");
+const getCoordinates = async (city, country) => {
+  api_key = "pk.68037dd88762a67a25d11841170d2b52";
+  const endpoint = "https://us1.locationiq.com/v1/search/structured";
+  let lat,
+    lon,
+    rate = 0;
+  const params = {
+    city: city,
+    country: country,
+    format: "json",
+    key: api_key,
+  };
+
+  try {
+    const response = await axios.get(endpoint, { params });
+    const places = Object.values(response.data).flat();
+    for (const place of places) {
+      if (place.importance > rate) {
+        rate = place.importance;
+        lat = place.lat;
+        lon = place.lon;
+      }
+    }
+
+    return `${lat},${lon}` ;
+  } catch (error) {
+    console.error(
+      "Error al hacer la solicitud a la API de TripAdvisor:",
+      error.message
+    );
+  }
+};
 
 module.exports = {
   fetchPlacesByType,
   placesDB,
-  getPlaceTripAvisor,
+  searchPlacesTripAdvisor,
   getNearbyAttractions,
+  getCoordinates,
+  geocodeGoogleMaps,
 };
